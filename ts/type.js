d8148bfbc62868e0850071d921f29992cc7c8613
pratikshaHattikatti <pratiksha.hattikatti@gmail.com>
2019-08-06 10:29:44 +0530

    js:mouse

---
+++ ts/type.js
@@ -0,0 +1,12 @@
+var nm = 'android';
+var num = 10;
+var isCV = false;
+var weight = 425.7;
+var obj = {
+    age: 5 == 5,
+    cb: true
+};
+var fn = function (n) {
+    return true;
+};
+var mixed = 'kkhhfy';
---
+++ ts/type.ts
@@ -0,0 +1,12 @@
+let nm = 'android'
+let num: number = 10
+let isCV: boolean = false
+let weight: number = 425.7
+let obj: {[key: string]: boolean} = {
+    age: 5 == 5,
+    cb: true
+}
+let fn: (n:number) => boolean = function (n: number){
+    return true
+}
+let mixed: number | string | {} | (() => void) = 'kkhhfy'
